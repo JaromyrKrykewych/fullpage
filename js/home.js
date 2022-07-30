726c7fda49b0d00ad6452f7d32bd81211e365e3b
Jaromyr Krykewych <krykewychjaromyr@gmail.com>
2022-07-29 23:31:20 -0300

    images added for the contact images and js upgraded

--- js/home.js
+++ js/home.js
@@ -31,7 +31,7 @@ const carouselFunction = (mainID, controlsID, targetsID, mainClass, targetClass)
   });
 };
 
-const targetSelector = (mainID, targetsID) => { /* mainID, targetsID, mainClass, targetClass */
+const targetSelector = (mainID, targetsID, targetClass) => { /* mainID, targetsID, mainClass, targetClass */
   let divTarget = document.getElementById(targetsID)
   let targets = [...divTarget.children]
   let divOptions = document.getElementById(mainID)
@@ -39,12 +39,45 @@ const targetSelector = (mainID, targetsID) => { /* mainID, targetsID, mainClass,
   
   targets.forEach(target => {
     target.addEventListener('click', e => {
-      if (e.target === targets[0] && !e.target.classList.contains('current')) {
-        console.log(1);
-      } else if(e.target === targets[1] && !e.target.classList.contains('current')) {
-        console.log(2);
-      } else if(e.target === targets[2] && !e.target.classList.contains('current')) {
-        console.log(3);
+      if (e.target === targets[0] && !e.target.classList.contains(targetClass)) {
+        for(let i=0; i<targets.length; i++){
+          options[i].classList.remove('active')
+          options[i].classList.remove('first')
+          options[i].classList.remove('third')
+          if(targets[i].classList.contains(targetClass)){
+            targets[i].classList.remove(targetClass)
+          }
+        }
+        e.target.classList.add(targetClass)
+        options[0].classList.add('active')
+        options[1].classList.add('third')
+        options[2].classList.add('first')
+      } else if(e.target === targets[1] && !e.target.classList.contains(targetClass)) {
+        for(let i=0; i<targets.length; i++){
+          options[i].classList.remove('active')
+          options[i].classList.remove('first')
+          options[i].classList.remove('third')
+          if(targets[i].classList.contains(targetClass)){
+            targets[i].classList.remove(targetClass)
+          }
+        }
+        e.target.classList.add(targetClass)
+        options[1].classList.add('active')
+        options[2].classList.add('third')
+        options[0].classList.add('first')
+      } else if(e.target === targets[2] && !e.target.classList.contains(targetClass)) {
+        for(let i=0; i<targets.length; i++){
+          options[i].classList.remove('active')
+          options[i].classList.remove('first')
+          options[i].classList.remove('third')
+          if(targets[i].classList.contains(targetClass)){
+            targets[i].classList.remove(targetClass)
+          }
+        }
+        e.target.classList.add(targetClass)
+        options[2].classList.add('active')
+        options[0].classList.add('third')
+        options[1].classList.add('first')
       }
     })
   })
@@ -59,7 +92,7 @@ carouselFunction("carousel-message", "message-controls", "message-targets", "act
 let mediaqueryDesktop = window.matchMedia("(min-width: 1024px)");
 
 if (mediaqueryDesktop.matches) {
-  targetSelector("carousel-proyects", "proyects-targets")
+  targetSelector("carousel-proyects", "proyects-targets", "current")
 } else {
   carouselFunction("carousel-proyects" , "proyects-controls", "proyects-targets", "active", "current")
 }
